487eac519b6b0e9ec4e4a47ca383223153879902
Isaac Annan Amoah <annanamoahisaac585@outlook.com>
2025-09-04 18:10:01 +0000

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,34 @@
+SELECT
+  p.property_id,
+  p.property_name,
+  p.location,
+  avg_ratings.average_rating
+FROM
+  Properties AS p
+INNER JOIN
+  (
+    SELECT
+      property_id,
+      AVG(rating) AS average_rating
+    FROM
+      Reviews
+    GROUP BY
+      property_id
+  ) AS avg_ratings ON p.property_id = avg_ratings.property_id
+WHERE
+  avg_ratings.average_rating > 4.0;
 
+
+
+SELECT
+  u.user_id,
+  u.username,
+  u.email
+FROM
+  Users AS u
+WHERE
+  (
+    SELECT COUNT(*)
+    FROM Bookings AS b
+    WHERE b.user_id = u.user_id
+  ) > 3;
